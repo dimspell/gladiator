a4cfd7d02a6e3bc4befb4db7dd363a9b2036a979
dispel-re <141545384+dispel-re@users.noreply.github.com>
2023-11-28 15:06:53 +0100

    Add client library

--- console/client/client.go
+++ console/client/client.go
@@ -1,6 +1,10 @@
 package client
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
 	"net/http"
 	"time"
 )
@@ -15,4 +19,49 @@ func New(consoleAddr string) *Client {
 		ConsoleAddr: consoleAddr,
 		HttpClient:  &http.Client{Timeout: 5 * time.Second},
 	}
+}
+
+func doRequest(
+	ctx context.Context,
+	client *http.Client,
+	method string,
+	url string,
+	headers map[string]string,
+	body any,
+) ([]byte, int, error) {
+	var bodyReader io.Reader
+	if body != nil {
+		if method == "POST" || method == "PUT" {
+			bodyBytes, err := json.Marshal(body)
+			if err != nil {
+				return nil, 0, err
+			}
+
+			bodyReader = bytes.NewBuffer(bodyBytes)
+		}
+	}
+
+	// Create the new request
+	req, err := http.NewRequestWithContext(ctx, method, url, bodyReader)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// Set request headers
+	for key, value := range headers {
+		req.Header.Add(key, value)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer resp.Body.Close()
+
+	payload, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return payload, resp.StatusCode, nil
 }
