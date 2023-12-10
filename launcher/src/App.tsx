baea3082c3422fb1feb2c979a6eca86bc7f3bf99
dispel-re <141545384+dispel-re@users.noreply.github.com>
2023-12-10 09:03:03 +0100

    Bootstrap tailwind, app intro page

--- launcher/src/App.tsx
+++ launcher/src/App.tsx
@@ -1,53 +1,94 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { useState } from 'react'
+import { invoke } from '@tauri-apps/api/tauri'
+import './App.css'
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState('')
+  const [name, setName] = useState('')
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }))
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    // <div className='container'>
+    //   <div className='row'>
+    //     <form
+    //       onSubmit={(e) => {
+    //         e.preventDefault()
+    //         greet()
+    //       }}
+    //     >
+    //       <input
+    //         id='greet-input'
+    //         onChange={(e) => setName(e.currentTarget.value)}
+    //         placeholder='Enter a name...'
+    //       />
+    //       <button type='submit'>Greet</button>
+    //     </form>
+    //   </div>
+    //   <p>{greetMsg}</p>
+    // </div>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-700 py-6 sm:py-12'>
+      <div
+        className='relative bg-gray-800 px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-100/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10'>
+        <div className='mx-auto max-w-md'>
+          <h1 className={'text-2xl text-gray-50'}>Dispel<span className={'font-bold'}>Multi</span></h1>
+          <div className='divide-y divide-gray-700/50'>
+            <div className='space-y-6 py-8 text-base leading-7 text-gray-300'>
+              <h2 className='font-bold text-gray-100'>Greetings, brave adventurer!</h2>
+              <p className='text-gray-100 leading-'>
+                Whether you're stepping into the mystical realms of Dman for the first time or returning for another epic
+                journey, we're thrilled to have you here. Prepare yourself for a world of magic, challenges, and
+                camaraderie.
+              </p>
+              <h2 className='font-bold text-gray-100'>Ready to Begin Your Journey?</h2>
+              <p className='text-gray-100 leading-'>
+                Follow the wizard to host your very own server or choose an existing server to join forces and forge
+                alliances as you embark on quests together.
+              </p>
+              <div className={'flex flex-row space-x-7'}>
+                <a href={'#'} className='flex-1 text-center text-sky-100 hover:text-sky-200 bg-sky-700 hover:bg-sky-800 px-5 py-4 rounded'>
+                  Join a Server
+                </a>
+                <a href={'#'} className='flex-1 text-center text-amber-100 hover:text-amber-200 bg-amber-700 hover:bg-amber-800 px-5 py-4 rounded'>
+                  Host a Server
+                </a>
+              </div>
+            </div>
+            <div className='pt-8 text-base font-semibold leading-7'>
+              <p className='text-gray-100'>Are you curious about the development?</p>
+              <p>
+                <a href='https://tailwindcss.com/docs' className='text-sky-500 hover:text-sky-600'>Join Discord channel &rarr;</a>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
+      <div className='mt-10'>
+        <p className='text-xs text-center text-gray-400'>
+          Version 0.0.1 | Build Date: {new Date().toDateString()}
+        </p>
       </div>
-      <p>{greetMsg}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+function routing() {
+  return (
+    <div about={'tauri-app'}>
+      <div about={'start-screen'}>
+        <div about={'console selection'}>
+          <div about={'Connect of existing server'}></div>
+          <div about={'Host a server'}></div>
+        </div>
+        <div about={'read more about the project'}></div>
+      </div>
+    </div>
+  )
+}
+
+
+export default App
