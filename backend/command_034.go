79450193bd4c2aa13d98598612d26a81b1e1b200
dispel-re <141545384+dispel-re@users.noreply.github.com>
2023-12-31 11:46:16 +0100

    Update 34

--- backend/command_034.go
+++ backend/command_034.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"net"
 
 	"connectrpc.com/connect"
 	multiv1 "github.com/dispel-re/dispel-multi/gen/multi/v1"
@@ -30,65 +31,65 @@ func (b *Backend) HandleJoinGame(session *model.Session, req JoinGameRequest) er
 	}
 
 	// clientIpAddress := session.Conn.RemoteAddr().(*net.TCPAddr).IP.To4().String()
-	// clientIpAddress := "192.168.121.212" // 169
+	clientIpAddress := "192.168.121.212" // 169
 	// clientIpAddress := "127.0.0.34"
 
-	// _, err = b.GameClient.JoinGame(context.TODO(), connect.NewRequest(&multiv1.JoinGameRequest{
-	// 	UserId:      session.UserID,
-	// 	CharacterId: session.CharacterID,
-	// 	GameRoomId:  respGame.Msg.Game.GameId,
-	// 	IpAddress:   clientIpAddress,
-	// }))
-	// if err != nil {
-	// 	return err
-	// }
-
-	gameRoom := model.GameRoom{
-		Lobby: model.LobbyRoom{
-			HostIPAddress: [4]byte{192, 168, 121, 169},
-			Name:          respGame.Msg.Game.Name,
-			Password:      "",
-		},
-		MapID: uint32(respGame.Msg.Game.GetMapId()),
-		Players: []model.LobbyPlayer{
-			{
-				ClassType: model.ClassType(model.ClassTypeArcher),
-				Name:      "character2", // tester
-				IPAddress: [4]byte{192, 168, 121, 169},
-			},
-			{
-				ClassType: model.ClassType(model.ClassTypeArcher),
-				Name:      "tester", // test
-				IPAddress: [4]byte{192, 168, 121, 212},
-			},
-		},
+	_, err = b.GameClient.JoinGame(context.TODO(), connect.NewRequest(&multiv1.JoinGameRequest{
+		UserId:      session.UserID,
+		CharacterId: session.CharacterID,
+		GameRoomId:  respGame.Msg.Game.GameId,
+		IpAddress:   clientIpAddress,
+	}))
+	if err != nil {
+		return err
 	}
 
 	// gameRoom := model.GameRoom{
 	// 	Lobby: model.LobbyRoom{
-	// 		HostIPAddress: [4]byte{},
+	// 		HostIPAddress: [4]byte{192, 168, 121, 169},
 	// 		Name:          respGame.Msg.Game.Name,
-	// 		Password:      respGame.Msg.Game.Password,
+	// 		Password:      "",
 	// 	},
 	// 	MapID: uint32(respGame.Msg.Game.GetMapId()),
+	// 	Players: []model.LobbyPlayer{
+	// 		{
+	// 			ClassType: model.ClassType(model.ClassTypeArcher),
+	// 			Name:      "character2", // tester
+	// 			IPAddress: [4]byte{192, 168, 121, 169},
+	// 		},
+	// 		{
+	// 			ClassType: model.ClassType(model.ClassTypeArcher),
+	// 			Name:      "tester", // test
+	// 			IPAddress: [4]byte{192, 168, 121, 212},
+	// 		},
+	// 	},
 	// }
-	// copy(gameRoom.Lobby.HostIPAddress[:], net.ParseIP(respGame.Msg.Game.HostIpAddress).To4())
-	// // gameRoom.Lobby.HostIPAddress = [4]byte{127, 21, 37, 28}
 
-	// respPlayers, err := b.GameClient.ListPlayers(context.TODO(), connect.NewRequest(&multiv1.ListPlayersRequest{
-	// 	GameRoomId: respGame.Msg.Game.GameId,
-	// }))
-	// if err != nil {
-	// 	return err
-	// }
-	// for _, player := range respPlayers.Msg.GetPlayers() {
-	// 	lobbyPlayer := model.LobbyPlayer{
-	// 		ClassType: model.ClassType(player.ClassType),
-	// 		Name:      player.CharacterName,
-	// 	}
-	// 	copy(lobbyPlayer.IPAddress[:], net.ParseIP(player.IpAddress).To4())
-	// 	gameRoom.Players = append(gameRoom.Players, lobbyPlayer)
-	// }
+	gameRoom := model.GameRoom{
+		Lobby: model.LobbyRoom{
+			HostIPAddress: [4]byte{},
+			Name:          respGame.Msg.Game.Name,
+			Password:      respGame.Msg.Game.Password,
+		},
+		MapID: uint32(respGame.Msg.Game.GetMapId()),
+	}
+	copy(gameRoom.Lobby.HostIPAddress[:], net.ParseIP(respGame.Msg.Game.HostIpAddress).To4())
+	// gameRoom.Lobby.HostIPAddress = [4]byte{127, 21, 37, 28}
+
+	respPlayers, err := b.GameClient.ListPlayers(context.TODO(), connect.NewRequest(&multiv1.ListPlayersRequest{
+		GameRoomId: respGame.Msg.Game.GameId,
+	}))
+	if err != nil {
+		return err
+	}
+	for _, player := range respPlayers.Msg.GetPlayers() {
+		lobbyPlayer := model.LobbyPlayer{
+			ClassType: model.ClassType(player.ClassType),
+			Name:      player.CharacterName,
+		}
+		copy(lobbyPlayer.IPAddress[:], net.ParseIP(player.IpAddress).To4())
+		gameRoom.Players = append(gameRoom.Players, lobbyPlayer)
+	}
 
 	return b.Send(session.Conn, JoinGame, gameRoom.Details())
 }
