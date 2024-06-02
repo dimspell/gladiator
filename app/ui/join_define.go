5b078ed2d0e1fa99ab354bd5ab60902559da499f
dispel-re <141545384+dispel-re@users.noreply.github.com>
2024-06-02 20:59:44 +0200

    Cosmetic changes to the join-define page

--- app/ui/join_define.go
+++ app/ui/join_define.go
@@ -18,23 +18,30 @@ func (c *Controller) JoinDefineScreen(w fyne.Window) fyne.CanvasObject {
 	value1.Validator = nil
 	value1.PlaceHolder = "Full address, for example http://127.0.0.1:2137"
 
-	headerText := "Join a server"
+	headerText := "Join a server - define auth server"
 
-	return container.NewPadded(container.NewVBox(
-		headerContainer(headerText, func() {
+	return container.NewBorder(
+		container.NewPadded(headerContainer(headerText, func() {
 			log.Println("Welcome")
 			w.SetContent(c.JoinOptionsScreen(w))
-		}),
-		widget.NewLabel(""),
+		})),
+		nil,
+		nil,
+		nil,
+		container.NewPadded(
+			container.NewVBox(
+				widget.NewLabel(""),
 
-		label1,
-		value1,
+				label1,
+				value1,
 
-		widget.NewLabel(""),
-		container.NewCenter(
-			widget.NewButtonWithIcon("Next", theme.NavigateNextIcon(), func() {
-				log.Println(str.Get())
-			}),
+				widget.NewLabel(""),
+				container.NewCenter(
+					widget.NewButtonWithIcon("Next", theme.NavigateNextIcon(), func() {
+						log.Println(str.Get())
+					}),
+				),
+			),
 		),
-	))
+	)
 }
