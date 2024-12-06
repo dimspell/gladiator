2be2583ba8ea67845f2099b47ae99a0206a97785
dimspell <141545384+dimspell@users.noreply.github.com>
2024-12-06 20:34:30 +0100

    Change test to use 2 separate backends

--- internal/backend/proxy_lan_test.go
+++ internal/backend/proxy_lan_test.go
@@ -3,6 +3,7 @@ package backend
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"log/slog"
 	"net/http/httptest"
 	"os"
@@ -47,14 +48,14 @@ func TestE2E_LAN(t *testing.T) {
 	// Remove the HTTP schema prefix
 	cs.Addr = ts.URL[len("http://"):]
 
-	bd := NewBackend("", cs.Addr, NewLAN("198.51.100.1"))
-	bd.SignalServerURL = "ws://" + cs.Addr + "/lobby"
+	bd1 := NewBackend("", cs.Addr, NewLAN("198.51.100.1"))
+	bd1.SignalServerURL = "ws://" + cs.Addr + "/lobby"
 
 	conn1 := &mockConn{}
-	session1 := bd.AddSession(conn1)
+	session1 := bd1.AddSession(conn1)
 
 	// Sign-in
-	assert.NoError(t, bd.HandleClientAuthentication(session1, ClientAuthenticationRequest{
+	assert.NoError(t, bd1.HandleClientAuthentication(session1, ClientAuthenticationRequest{
 		2, 0, 0, 0, // Unknown
 		't', 'e', 's', 't', 0, // Password
 		'a', 'r', 'c', 'h', 'e', 'r', 0, // Username
@@ -65,29 +66,29 @@ func TestE2E_LAN(t *testing.T) {
 	}
 
 	// Select character
-	assert.NoError(t, bd.HandleSelectCharacter(session1, SelectCharacterRequest{
+	assert.NoError(t, bd1.HandleSelectCharacter(session1, SelectCharacterRequest{
 		'a', 'r', 'c', 'h', 'e', 'r', 0, // User name
 		'a', 'r', 'c', 'h', 'e', 'r', 0, // Character name
 	}))
-	err = bd.JoinLobby(ctx, session1)
+	err = bd1.JoinLobby(ctx, session1)
 	if err != nil {
 		t.Errorf("failed to join lobby: %v", err)
 		return
 	}
-	err = bd.RegisterNewObserver(ctx, session1)
+	err = bd1.RegisterNewObserver(ctx, session1)
 	if err != nil {
 		t.Errorf("failed to register new observer: %v", err)
 		return
 	}
 
 	// Create new game room
-	assert.NoError(t, bd.HandleCreateGame(session1, CreateGameRequest{
+	assert.NoError(t, bd1.HandleCreateGame(session1, CreateGameRequest{
 		0, 0, 0, 0, // State
 		byte(v1.GameMap_FrozenLabyrinth), 0, 0, 0, // Map ID
 		'r', 'o', 'o', 'm', 0, // Game room name
 		0, // Password
 	}))
-	assert.NoError(t, bd.HandleCreateGame(session1, CreateGameRequest{
+	assert.NoError(t, bd1.HandleCreateGame(session1, CreateGameRequest{
 		1, 0, 0, 0, // State
 		byte(v1.GameMap_FrozenLabyrinth), 0, 0, 0, // Map ID
 		'r', 'o', 'o', 'm', 0, // Game room name
@@ -115,10 +116,14 @@ func TestE2E_LAN(t *testing.T) {
 
 	// Other user
 	conn2 := &mockConn{}
-	session2 := bd.AddSession(conn2)
+
+	bd2 := NewBackend("", cs.Addr, NewLAN("198.51.100.2"))
+	bd2.SignalServerURL = "ws://" + cs.Addr + "/lobby"
+
+	session2 := bd2.AddSession(conn2)
 
 	// Sign-in by player2
-	assert.NoError(t, bd.HandleClientAuthentication(session2, ClientAuthenticationRequest{
+	assert.NoError(t, bd2.HandleClientAuthentication(session2, ClientAuthenticationRequest{
 		2, 0, 0, 0, // Unknown
 		't', 'e', 's', 't', 0, // Password
 		'm', 'a', 'g', 'e', 0, // Username
@@ -129,16 +134,16 @@ func TestE2E_LAN(t *testing.T) {
 	}
 
 	// Select character by player2
-	assert.NoError(t, bd.HandleSelectCharacter(session2, SelectCharacterRequest{
+	assert.NoError(t, bd2.HandleSelectCharacter(session2, SelectCharacterRequest{
 		'm', 'a', 'g', 'e', 0, // User name
 		'm', 'a', 'g', 'e', 0, // Character name
 	}))
-	err = bd.JoinLobby(ctx, session2)
+	err = bd2.JoinLobby(ctx, session2)
 	if err != nil {
 		t.Errorf("failed to join lobby: %v", err)
 		return
 	}
-	err = bd.RegisterNewObserver(ctx, session2)
+	err = bd2.RegisterNewObserver(ctx, session2)
 	if err != nil {
 		t.Errorf("failed to register new observer: %v", err)
 		return
@@ -148,7 +153,7 @@ func TestE2E_LAN(t *testing.T) {
 	conn2.Written = nil
 
 	// List games
-	assert.NoError(t, bd.HandleListGames(session2, ListGamesRequest{}))
+	assert.NoError(t, bd2.HandleListGames(session2, ListGamesRequest{}))
 
 	// Check if user has received the game list with corresponding payload
 	assert.Equal(t, []byte{
@@ -162,7 +167,7 @@ func TestE2E_LAN(t *testing.T) {
 	conn2.Written = nil
 
 	// Select game
-	assert.NoError(t, bd.HandleSelectGame(session2, SelectGameRequest{
+	assert.NoError(t, bd2.HandleSelectGame(session2, SelectGameRequest{
 		'r', 'o', 'o', 'm', 0, // Game name
 		0, // Password
 	}))
@@ -179,12 +184,10 @@ func TestE2E_LAN(t *testing.T) {
 	conn2.Written = nil
 
 	// Join to host
-	assert.NoError(t, bd.HandleJoinGame(session2, JoinGameRequest{
+	assert.NoError(t, bd2.HandleJoinGame(session2, JoinGameRequest{
 		'r', 'o', 'o', 'm', 0, // Game name
 		0, // Password
 	}))
-
-	// bd.Proxy = NewLAN("198.51.100.105")
 
 	// Ensure the response is correct
 	assert.Equal(t, []byte{
@@ -223,7 +226,7 @@ func TestE2E_LAN(t *testing.T) {
 	assert.Equal(t, "198.51.100.1", session1.gameRoom.Players["1"].IPAddress)
 	assert.Equal(t, "mage", session1.gameRoom.Players["2"].Username)
 	assert.Equal(t, byte(v1.ClassType_Mage), session1.gameRoom.Players["2"].ClassType)
-	assert.Equal(t, "198.51.100.1", session1.gameRoom.Players["2"].IPAddress)
+	assert.Equal(t, "198.51.100.2", session1.gameRoom.Players["2"].IPAddress)
 
 	// Joining user has also the same data
 	assert.Equal(t, int64(2), session2.UserID)
@@ -233,12 +236,12 @@ func TestE2E_LAN(t *testing.T) {
 	assert.Equal(t, "198.51.100.1", session2.gameRoom.Players["1"].IPAddress)
 	assert.Equal(t, "mage", session2.gameRoom.Players["2"].Username)
 	assert.Equal(t, byte(v1.ClassType_Mage), session2.gameRoom.Players["2"].ClassType)
-	assert.Equal(t, "198.51.100.1", session2.gameRoom.Players["2"].IPAddress)
+	assert.Equal(t, "198.51.100.2", session2.gameRoom.Players["2"].IPAddress)
 
-	// close(cs.Multiplayer.Messages)
-	// for message := range cs.Multiplayer.Messages {
-	// 	fmt.Println("unhandled message", message)
-	// }
+	close(cs.Multiplayer.Messages)
+	for message := range cs.Multiplayer.Messages {
+		fmt.Println("unhandled message", message)
+	}
 }
 
 func findPacket(buf []byte, packetType PacketType) []byte {
