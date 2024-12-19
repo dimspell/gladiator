59fa4e2e9aaf00ce072c2d86b3f2b2ff7e0dda1d
dimspell <141545384+dimspell@users.noreply.github.com>
2024-12-19 11:02:08 +0100

    Remove unused stuff

--- internal/backend/proxy_p2p_test.go
+++ internal/backend/proxy_p2p_test.go
@@ -3,10 +3,14 @@ package backend
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"log"
 	"log/slog"
+	"net"
 	"net/http/httptest"
 	"os"
 	"testing"
+	"time"
 
 	v1 "github.com/dimspell/gladiator/gen/multi/v1"
 	"github.com/dimspell/gladiator/internal/app/logger"
@@ -49,7 +53,7 @@ func TestE2E_P2P(t *testing.T) {
 	cs.Addr = ts.URL[len("http://"):]
 
 	proxy1 := NewPeerToPeer()
-	proxy1.NewRedirect = redirect.NewNoop
+	proxy1.NewRedirect = redirect.New
 	bd1 := NewBackend("", cs.Addr, proxy1)
 	bd1.SignalServerURL = "ws://" + cs.Addr + "/lobby"
 
@@ -119,7 +123,7 @@ func TestE2E_P2P(t *testing.T) {
 
 	// Other user
 	proxy2 := NewPeerToPeer()
-	proxy2.NewRedirect = redirect.NewNoop
+	proxy2.NewRedirect = redirect.New
 	bd2 := NewBackend("", cs.Addr, proxy2)
 	bd2.SignalServerURL = "ws://" + cs.Addr + "/lobby"
 
@@ -265,4 +269,106 @@ func TestE2E_P2P(t *testing.T) {
 	}
 
 	// select {}
+}
+
+func helperStartGameServer(t testing.TB) {
+	t.Helper()
+
+	ctx, cancel := context.WithCancel(context.TODO())
+
+	// Listen for incoming connections.
+	tcpListener, err := net.Listen("tcp", net.JoinHostPort("127.0.0.1", "6114"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	udpAddr, err := net.ResolveUDPAddr("udp", net.JoinHostPort("127.0.0.1", "6113"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Listen UDP
+	go func() {
+		for {
+			if ctx.Err() != nil {
+				fmt.Println("context err")
+				return
+			}
+
+			buf := make([]byte, 1024)
+			n, _, err := udpConn.ReadFrom(buf)
+			if err != nil {
+				break
+			}
+
+			if buf[0] == '#' {
+				resp := append([]byte{27, 0}, buf[1:n]...)
+				_, err := udpConn.WriteToUDP(resp, udpAddr)
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				log.Println("UDP response", string(resp))
+			}
+		}
+	}()
+
+	processPackets := func(conn net.Conn) {
+		message := make(chan []byte, 1)
+
+		go func() {
+			defer conn.Close()
+
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case msg, ok := <-message:
+					if !ok {
+						return
+					}
+					log.Println("Message received", string(msg))
+					conn.Write([]byte{35, 35, 116, 101, 115, 116, 0})
+				}
+			}
+		}()
+
+		for {
+			conn.SetDeadline(time.Now().Add(10 * time.Second))
+
+			buf := make([]byte, 1024)
+			n, err := conn.Read(buf)
+			if err != nil {
+				close(message)
+				return
+			}
+			message <- buf[:n]
+		}
+	}
+
+	go func() {
+		for {
+			if ctx.Err() != nil {
+				return
+			}
+
+			// Listen for an incoming connection.
+			conn, err := tcpListener.Accept()
+			if err != nil {
+				continue
+			}
+			go processPackets(conn)
+		}
+	}()
+
+	t.Cleanup(func() {
+		cancel()
+		udpConn.Close()
+		tcpListener.Close()
+	})
 }
