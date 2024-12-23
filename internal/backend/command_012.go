96e9cf583ef7e539d321c0608538d61502690aab
dimspell <141545384+dimspell@users.noreply.github.com>
2024-12-23 16:04:37 +0100

    Send the lobby list every time the command 12 is sent

--- internal/backend/command_012.go
+++ internal/backend/command_012.go
@@ -1,12 +1,19 @@
 package backend
 
 import (
-	"bytes"
+	"fmt"
+	"github.com/dimspell/gladiator/internal/backend/packet"
+	"github.com/dimspell/gladiator/internal/model"
+	"log/slog"
 )
 
 func (b *Backend) HandleSelectChannel(session *Session, req SelectChannelRequest) error {
-	channelName, _ := req.Parse()
-	if channelName == "DISPEL" {
+	serverName, channelName, err := req.Parse()
+	slog.Info("Selected channel", "serverName", serverName, "channelName", channelName, "error", err)
+	if serverName == "DISPEL" && channelName == "DISPEL" {
+		for idx, user := range session.lobbyUsers {
+			b.Send(session.Conn, ReceiveMessage, AppendCharacterToLobby(user.Username, model.ClassType(user.ClassType), uint32(idx)))
+		}
 		//b.Send(session.Conn, ReceiveMessage, NewGlobalMessage("admin", "hello"))
 	}
 
@@ -22,7 +29,15 @@ func (b *Backend) HandleSelectChannel(session *Session, req SelectChannelRequest
 
 type SelectChannelRequest []byte
 
-func (r SelectChannelRequest) Parse() (string, error) {
-	split := bytes.Split(r, []byte{0})
-	return string(split[0]), nil
+func (r SelectChannelRequest) Parse() (serverName string, channelName string, err error) {
+	rd := packet.NewReader(r)
+	serverName, err = rd.ReadString()
+	if err != nil {
+		return "", "", fmt.Errorf("error parsing server name: %w", err)
+	}
+	channelName, err = rd.ReadString()
+	if err != nil {
+		return "", "", fmt.Errorf("error parsing server name: %w", err)
+	}
+	return serverName, channelName, err
 }
