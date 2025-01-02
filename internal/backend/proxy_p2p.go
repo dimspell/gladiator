16abb91eb68ae916cf5bc15f4fdd1f610a91b031
dimspell <141545384+dimspell@users.noreply.github.com>
2025-01-02 17:45:41 +0100

    Try to write code to be more testable

--- internal/backend/proxy_p2p.go
+++ internal/backend/proxy_p2p.go
@@ -179,87 +179,11 @@ func (p *PeerToPeer) ExtendWire(session *Session) MessageHandler {
 	}
 }
 
-type PeerToPeerMessageHandler struct {
-	session *Session
-	proxy   *PeerToPeer
-}
+type PeerToPeerInterface interface {
+	getPeer(session *Session, peerId string) (*p2p.Peer, bool)
+	deletePeer(session *Session, peerId string)
 
-func (p *PeerToPeerMessageHandler) Handle(ctx context.Context, payload []byte) error {
-	et := wire.ParseEventType(payload)
-
-	switch et {
-	case wire.JoinRoom:
-		return p.handleJoinRoom(payload)
-	case wire.RTCOffer:
-		return p.handleRTCOffer(payload)
-	case wire.RTCAnswer:
-		return p.handleRTCAnswer(payload)
-	case wire.RTCICECandidate:
-		return p.handleRTCCandidate(payload)
-	case wire.LeaveRoom, wire.LeaveLobby:
-		return p.handleLeaveRoom(payload)
-	default:
-		slog.Debug("unknown wire message", slog.String("type", et.String()), slog.String("payload", string(payload)))
-		return nil
-	}
-}
-
-func (p *PeerToPeerMessageHandler) handleJoinRoom(payload []byte) error {
-	_, msg, err := wire.DecodeTyped[wire.Player](payload)
-	if err != nil {
-		slog.Error("failed to decode join room payload", "error", err, "payload", string(payload))
-		return nil
-	}
-
-	player := msg.Content
-	slog.Info("Other player is joining", "playerId", player.ID())
-	p.session.State.GameRoom().SetPlayer(player)
-
-	return p.proxy.handleJoinRoom(msg, p.session)
-}
-
-func (p *PeerToPeerMessageHandler) handleRTCOffer(payload []byte) error {
-	_, msg, err := wire.DecodeTyped[wire.Offer](payload)
-	if err != nil {
-		slog.Error("failed to decode RTC Offer payload", "error", err, "payload", string(payload))
-		return nil
-	}
-
-	return p.proxy.handleRTCOffer(msg, p.session)
-}
-
-func (p *PeerToPeerMessageHandler) handleRTCAnswer(payload []byte) error {
-	_, msg, err := wire.DecodeTyped[wire.Offer](payload)
-	if err != nil {
-		slog.Error("failed to decode RTC Answer payload", "error", err, "payload", string(payload))
-		return nil
-	}
-
-	return p.proxy.handleRTCAnswer(msg, p.session)
-}
-
-func (p *PeerToPeerMessageHandler) handleRTCCandidate(payload []byte) error {
-	_, msg, err := wire.DecodeTyped[webrtc.ICECandidateInit](payload)
-	if err != nil {
-		slog.Error("failed to decode RTC ICE Candidate payload", "error", err, "payload", string(payload))
-		return nil
-	}
-
-	return p.proxy.handleRTCCandidate(msg, p.session)
-}
-
-func (p *PeerToPeerMessageHandler) handleLeaveRoom(payload []byte) error {
-	_, msg, err := wire.DecodeTyped[wire.Player](payload)
-	if err != nil {
-		slog.Error("failed to decode leave-room/leave-lobby payload", "error", err, "payload", string(payload))
-		return nil
-	}
-
-	player := msg.Content
-	slog.Info("Other player is leaving", "playerId", player.ID())
-	p.session.State.GameRoom().DeletePlayer(player)
-
-	return p.proxy.handleLeaveRoom(msg, p.session)
+	setUpChannels(session *Session, peerId int64, sendRTCOffer bool, createChannels bool) (*p2p.Peer, error)
 }
 
 func (p *PeerToPeer) getPeer(session *Session, peerID string) (*p2p.Peer, bool) {
@@ -289,100 +213,6 @@ func (p *PeerToPeer) deletePeer(session *Session, peerID string) {
 		return
 	}
 	delete(mapping.Peers, peerID)
-}
-
-func (p *PeerToPeer) handleJoinRoom(m wire.MessageContent[wire.Player], session *Session) error {
-	// Validate the message
-	if m.Content.UserID == session.UserID {
-		return nil
-	}
-
-	peer, connected := p.getPeer(session, m.Content.ID())
-	if connected && peer.Connection != nil {
-		slog.Debug("Peer already exists, ignoring join", "userId", m.Content.UserID)
-		return nil
-	}
-
-	slog.Debug("JOIN", "id", m.Content.UserID, "data", m)
-
-	// Add the peer to the list of peers, and start the WebRTC connection
-	if _, err := p.setUpChannels(session, m.Content.UserID, true, true); err != nil {
-		slog.Warn("Could not add a peer", "userId", m.Content.UserID, "error", err)
-		return err
-	}
-
-	return nil
-}
-
-func (p *PeerToPeer) handleLeaveRoom(m wire.MessageContent[wire.Player], session *Session) error {
-	slog.Debug("LEAVE", "from", m.From, "to", m.To)
-
-	peer, ok := p.getPeer(session, m.From)
-	if !ok {
-		// fmt.Errorf("could not find peer %q", m.From)
-		return nil
-	}
-	if peer.PeerUserID == session.GetUserID() {
-		// return fmt.Errorf("peer %q is the same as the host, ignoring leave", m.From)
-		return nil
-	}
-
-	slog.Info("User left", "peer", peer.PeerUserID)
-	p.deletePeer(session, m.From)
-	return nil
-}
-
-func (p *PeerToPeer) handleRTCOffer(m wire.MessageContent[wire.Offer], session *Session) error {
-	slog.Debug("RTC_OFFER", "from", m.From, "to", m.To)
-
-	peer, err := p.setUpChannels(session, m.Content.UserID, false, false)
-	if err != nil {
-		return err
-	}
-
-	if err := peer.Connection.SetRemoteDescription(m.Content.Offer); err != nil {
-		return fmt.Errorf("could not set remote description: %v", err)
-	}
-
-	answer, err := peer.Connection.CreateAnswer(nil)
-	if err != nil {
-		return fmt.Errorf("could not create answer: %v", err)
-	}
-
-	if err := peer.Connection.SetLocalDescription(answer); err != nil {
-		return fmt.Errorf("could not set local description: %v", err)
-	}
-
-	response := wire.ComposeTyped[wire.Offer](wire.RTCAnswer, wire.MessageContent[wire.Offer]{
-		From: session.GetUserID(),
-		To:   m.From,
-		Type: wire.RTCAnswer,
-		Content: wire.Offer{
-			UserID: session.UserID, // TODO: Unused data
-			Offer:  answer,
-		},
-	})
-	if err := wire.Write(context.TODO(), session.wsConn, response); err != nil {
-		return fmt.Errorf("could not send answer: %v", err)
-	}
-	return nil
-}
-
-func (p *PeerToPeer) handleRTCAnswer(m wire.MessageContent[wire.Offer], session *Session) error {
-	slog.Debug("RTC_ANSWER", "from", m.From, "to", m.To)
-
-	answer := webrtc.SessionDescription{
-		Type: webrtc.SDPTypeAnswer,
-		SDP:  m.Content.Offer.SDP,
-	}
-	peer, ok := p.getPeer(session, m.From)
-	if !ok {
-		return fmt.Errorf("could not find peer %q that sent the RTC answer", m.From)
-	}
-	if err := peer.Connection.SetRemoteDescription(answer); err != nil {
-		return fmt.Errorf("could not set remote description: %v", err)
-	}
-	return nil
 }
 
 func (p *PeerToPeer) setUpChannels(session *Session, playerId int64, sendRTCOffer bool, createChannels bool) (*p2p.Peer, error) {
@@ -528,12 +358,170 @@ func (p *PeerToPeer) createDataChannel(peerConnection *webrtc.PeerConnection, la
 	return nil
 }
 
-func (p *PeerToPeer) handleRTCCandidate(m wire.MessageContent[webrtc.ICECandidateInit], session *Session) error {
-	slog.Debug("RTC_ICE_CANDIDATE", "from", m.From, "to", m.To)
+type PeerToPeerMessageHandler struct {
+	session *Session
+	proxy   PeerToPeerInterface
+}
 
-	peer, ok := p.getPeer(session, m.From)
-	if !ok {
-		return fmt.Errorf("could not find peer %q", m.From)
+func (p *PeerToPeerMessageHandler) Handle(ctx context.Context, payload []byte) error {
+	et := wire.ParseEventType(payload)
+
+	switch et {
+	case wire.JoinRoom:
+		return p.handleJoinRoom(payload)
+	case wire.RTCOffer:
+		return p.handleRTCOffer(payload)
+	case wire.RTCAnswer:
+		return p.handleRTCAnswer(payload)
+	case wire.RTCICECandidate:
+		return p.handleRTCCandidate(payload)
+	case wire.LeaveRoom, wire.LeaveLobby:
+		return p.handleLeaveRoom(payload)
+	default:
+		slog.Debug("unknown wire message", slog.String("type", et.String()), slog.String("payload", string(payload)))
+		return nil
 	}
-	return peer.Connection.AddICECandidate(m.Content)
+}
+
+func (p *PeerToPeerMessageHandler) handleJoinRoom(payload []byte) error {
+	_, msg, err := wire.DecodeTyped[wire.Player](payload)
+	if err != nil {
+		slog.Error("failed to decode join room payload", "error", err, "payload", string(payload))
+		return nil
+	}
+
+	player := msg.Content
+	slog.Info("Other player is joining", "playerId", player.ID())
+	p.session.State.GameRoom().SetPlayer(player)
+
+	// Validate the message
+	if msg.Content.UserID == p.session.UserID {
+		return nil
+	}
+
+	peer, connected := p.proxy.getPeer(p.session, player.ID())
+	if connected && peer.Connection != nil {
+		slog.Debug("Peer already exists, ignoring join", "userId", player.UserID)
+		return nil
+	}
+
+	slog.Debug("JOIN", "id", player.UserID, "data", msg)
+
+	// Add the peer to the list of peers, and start the WebRTC connection
+	if _, err := p.proxy.setUpChannels(p.session, player.UserID, true, true); err != nil {
+		slog.Warn("Could not add a peer", "userId", player.UserID, "error", err)
+		return err
+	}
+
+	return nil
+}
+
+func (p *PeerToPeerMessageHandler) handleRTCOffer(payload []byte) error {
+	_, msg, err := wire.DecodeTyped[wire.Offer](payload)
+	if err != nil {
+		slog.Error("failed to decode RTC Offer payload", "error", err, "payload", string(payload))
+		return nil
+	}
+
+	slog.Debug("RTC_OFFER", "from", msg.From, "to", msg.To)
+
+	peer, err := p.proxy.setUpChannels(p.session, msg.Content.UserID, false, false)
+	if err != nil {
+		return err
+	}
+
+	if err := peer.Connection.SetRemoteDescription(msg.Content.Offer); err != nil {
+		return fmt.Errorf("could not set remote description: %v", err)
+	}
+
+	answer, err := peer.Connection.CreateAnswer(nil)
+	if err != nil {
+		return fmt.Errorf("could not create answer: %v", err)
+	}
+
+	if err := peer.Connection.SetLocalDescription(answer); err != nil {
+		return fmt.Errorf("could not set local description: %v", err)
+	}
+
+	response := wire.ComposeTyped[wire.Offer](wire.RTCAnswer, wire.MessageContent[wire.Offer]{
+		From: p.session.GetUserID(),
+		To:   msg.From,
+		Type: wire.RTCAnswer,
+		Content: wire.Offer{
+			UserID: p.session.UserID, // TODO: Unused data
+			Offer:  answer,
+		},
+	})
+	if err := wire.Write(context.TODO(), p.session.wsConn, response); err != nil {
+		return fmt.Errorf("could not send answer: %v", err)
+	}
+	return nil
+}
+
+func (p *PeerToPeerMessageHandler) handleRTCAnswer(payload []byte) error {
+	_, msg, err := wire.DecodeTyped[wire.Offer](payload)
+	if err != nil {
+		slog.Error("failed to decode RTC Answer payload", "error", err, "payload", string(payload))
+		return nil
+	}
+
+	slog.Debug("RTC_ANSWER", "from", msg.From, "to", msg.To)
+
+	answer := webrtc.SessionDescription{
+		Type: webrtc.SDPTypeAnswer,
+		SDP:  msg.Content.Offer.SDP,
+	}
+	peer, ok := p.proxy.getPeer(p.session, msg.From)
+	if !ok {
+		return fmt.Errorf("could not find peer %q that sent the RTC answer", msg.From)
+	}
+	if err := peer.Connection.SetRemoteDescription(answer); err != nil {
+		return fmt.Errorf("could not set remote description: %v", err)
+	}
+	return nil
+}
+
+func (p *PeerToPeerMessageHandler) handleRTCCandidate(payload []byte) error {
+	_, msg, err := wire.DecodeTyped[webrtc.ICECandidateInit](payload)
+	if err != nil {
+		slog.Error("failed to decode RTC ICE Candidate payload", "error", err, "payload", string(payload))
+		return nil
+	}
+
+	slog.Debug("RTC_ICE_CANDIDATE", "from", msg.From, "to", msg.To)
+
+	peer, ok := p.proxy.getPeer(p.session, msg.From)
+	if !ok {
+		return fmt.Errorf("could not find peer %q", msg.From)
+	}
+
+	return peer.Connection.AddICECandidate(msg.Content)
+}
+
+func (p *PeerToPeerMessageHandler) handleLeaveRoom(payload []byte) error {
+	_, msg, err := wire.DecodeTyped[wire.Player](payload)
+	if err != nil {
+		slog.Error("failed to decode leave-room/leave-lobby payload", "error", err, "payload", string(payload))
+		return nil
+	}
+
+	player := msg.Content
+	slog.Info("Other player is leaving", "playerId", player.ID())
+	p.session.State.GameRoom().DeletePlayer(player)
+
+	slog.Debug("LEAVE", "from", msg.From, "to", msg.To)
+
+	peer, ok := p.proxy.getPeer(p.session, msg.From)
+	if !ok {
+		// fmt.Errorf("could not find peer %q", m.From)
+		return nil
+	}
+	if peer.PeerUserID == p.session.GetUserID() {
+		// return fmt.Errorf("peer %q is the same as the host, ignoring leave", m.From)
+		return nil
+	}
+
+	slog.Info("User left", "peer", peer.PeerUserID)
+	p.proxy.deletePeer(p.session, msg.From)
+	return nil
 }
