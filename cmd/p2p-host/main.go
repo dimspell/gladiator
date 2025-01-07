0c245aa601540337176d69a52f7f784852855d73
dimspell <141545384+dimspell@users.noreply.github.com>
2025-01-07 17:42:35 +0100

    Remove duplicated code

--- cmd/p2p-host/main.go
+++ cmd/p2p-host/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"log/slog"
-	"net"
 	"net/http"
 	"os"
 	"time"
@@ -99,85 +97,5 @@ func main() {
 	}
 	slog.Info("created a host room over proxy")
 
-	helperStartGameServer()
-}
-
-func helperStartGameServer() {
-	// Listen for incoming connections.
-	tcpListener, err := net.Listen("tcp", net.JoinHostPort("127.0.0.1", "6114"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	udpAddr, err := net.ResolveUDPAddr("udp", net.JoinHostPort("127.0.0.1", "6113"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	udpConn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Listen UDP
-	go func() {
-		for {
-			buf := make([]byte, 1024)
-			n, _, err := udpConn.ReadFrom(buf)
-			if err != nil {
-				break
-			}
-
-			if buf[0] == '#' {
-				resp := append([]byte{27, 0}, buf[1:n]...)
-				_, err := udpConn.WriteToUDP(resp, udpAddr)
-				if err != nil {
-					slog.Debug("Failed to write to UDP", "error", err)
-					return
-				}
-				slog.Debug("UDP response", "response", string(resp))
-			}
-		}
-	}()
-
-	processPackets := func(conn net.Conn) {
-		slog.Info("Someone has connected over the TCP")
-
-		message := make(chan []byte, 1)
-
-		go func() {
-			defer conn.Close()
-
-			for {
-				select {
-				case msg, ok := <-message:
-					if !ok {
-						return
-					}
-					slog.Debug("message received", "msg", string(msg))
-					conn.Write([]byte{35, 35, 116, 101, 115, 116, 0})
-				}
-			}
-		}()
-
-		for {
-			buf := make([]byte, 1024)
-			n, err := conn.Read(buf)
-			if err != nil {
-				close(message)
-				return
-			}
-			message <- buf[:n]
-		}
-	}
-
-	// ctx, cancel := context.WithCancel(context.TODO())
-	for {
-		// Listen for an incoming connection.
-		conn, err := tcpListener.Accept()
-		if err != nil {
-			continue
-		}
-		go processPackets(conn)
-	}
+	select {}
 }
