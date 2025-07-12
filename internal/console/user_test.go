9360721bcda3fa996499d07356d5a2f68a314758
dimspell <141545384+dimspell@users.noreply.github.com>
2025-07-12 22:31:28 +0200

    Add test if after creation of the account it is possible to sign in

---
+++ internal/console/user_test.go
@@ -0,0 +1,38 @@
+package console
+
+import (
+	"testing"
+
+	"connectrpc.com/connect"
+	multiv1 "github.com/dimspell/gladiator/gen/multi/v1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserServiceHandler(t *testing.T) {
+	t.Run("create user and sign in", func(t *testing.T) {
+		service := &userServiceServer{DB: setupDatabase(t)}
+
+		res, err := service.CreateUser(t.Context(), connect.NewRequest(&multiv1.CreateUserRequest{
+			Username: "testuser",
+			Password: "password",
+		}))
+		if err != nil {
+			t.Fatalf("create user failed: %v", err)
+			return
+		}
+
+		res2, err := service.AuthenticateUser(t.Context(), connect.NewRequest(&multiv1.AuthenticateUserRequest{
+			Username: "testuser",
+			Password: "password",
+		}))
+		if err != nil {
+			t.Fatalf("authenticate user failed: %v", err)
+			return
+		}
+
+		assert.Equal(t, int64(1), res.Msg.User.UserId)
+		assert.Equal(t, "testuser", res.Msg.User.Username)
+		assert.Equal(t, int64(1), res2.Msg.User.UserId)
+		assert.Equal(t, "testuser", res2.Msg.User.Username)
+	})
+}
