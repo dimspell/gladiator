9bfae94c8acb51a70b5d6e988d807fbbb1039afc
dimspell <141545384+dimspell@users.noreply.github.com>
2026-07-16 20:47:18 +0200

    fix(redirect): Correct NewTCPRedirect log to say TCP not UDP

--- internal/backend/redirect/redirect.go
+++ internal/backend/redirect/redirect.go
@@ -101,12 +101,12 @@ func NewUDPRedirect(joinType Mode, addr *Addressing) (Redirect, error) {
 }
 
 func NewTCPRedirect(joinType Mode, addr *Addressing) (Redirect, error) {
-	logger := slog.With(
+		logger := slog.With(
 		slog.String("redirect", "NewTCPRedirect"),
 		slog.String("joinType", joinType.String()),
 		slog.String("ip", addr.IP.String()),
 		slog.String("tcpPort", addr.TCPPort))
-	logger.Debug("Creating new UDP redirect")
+	logger.Debug("Creating new TCP redirect")
 
 	switch joinType {
 	case CurrentUserIsHost:
